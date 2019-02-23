bdc2be6e2be755ff6ff6221a9dbcceae22857468
Ethan <ethan.r.davidson@gmail.com>
2019-02-22 20:26:31 -0800

    edited asc2chr and reorganized data files

--- asc2chr.py
+++ asc2chr.py
@@ -1,19 +1,32 @@
+import sys
+
 # Helpers
-def longestString(strs):
+def longest_string(strs):
     longest = strs[0]
     for str in strs:
-        curLen, lonLen = len(str), len(longest)
-        if curLen > lonLen:
+        cur_len, lon_len = len(str), len(longest)
+        if cur_len > lon_len:
             longest = str
-    return longest
+    return len(longest)
 
-class CharNumber:
+class Char_Numbers:
+
     def __init__(self, lines):
         self.lines = lines
-        self.cols = longestString(lines)
-        self.rows = len(lines)
+        self.cols = longest_string(lines)
+
+    def to_string(self):
+        result = ""
+        for line in self.lines:
+            for i, char in enumerate(line):
+                result += str(ord(char)) + "\n"
+            if len(line) < self.cols:
+                result += "32\n" * (self.cols - len(line))
+        return result
 
 # Main Process
-gimmePath = "data/nums-hor.asc"
-with open(gimmePath, "r") as f:
+gimme_path = sys.argv[1]
+save_path = ""
+with open(gimme_path, "r") as f:
     lines = f.read().split("\n")
+    char_nums = Char_Numbers(lines)
